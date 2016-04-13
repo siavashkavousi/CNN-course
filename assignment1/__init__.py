18f67e95e65690039a3b3921bd8fd340d33fdde6
Siavash Kavousi <siavash.kavousi.sk@gmail.com>
2016-04-14 02:57:06 +0430

    Makes KNN, SVM, Softmax train and predict caller, function! - Adds measure elapsed time function

--- assignment1/__init__.py
+++ assignment1/__init__.py
@@ -1,10 +1,40 @@
 import data_utils
 from classifiers.linear_classifier import *
 from classifiers.k_nearest_neighbor import *
+from timeit import default_timer as timer
 
-X, y = data_utils.load_CIFAR_batch('datasets\cifar-10\data_batch_1')
-Xte, yte = data_utils.load_CIFAR_batch('datasets\cifar-10\\test_batch')
-# X, y, Xte, yte = data_utils.load_CIFAR10('datasets\cifar-10')
+
+def run_knn():
+    nn = KNearestNeighbor()
+    nn.train(X, y)
+    y_pred = nn.predict(Xte)
+    print 'accuracy: %f' % (np.mean(y_pred == yte))
+
+
+def run_svm():
+    lc = LinearSVM()
+    lc.train(X, y, batch_size=200, num_iters=10000)
+    y_pred = lc.predict(Xte)
+    print 'accuracy: %f' % (np.mean(y_pred == yte))
+
+
+def run_softmax():
+    sm = Softmax()
+    sm.train(X, y, num_iters=500)
+    y_pred = sm.predict(Xte)
+    print 'accuracy: %f' % (np.mean(y_pred == yte))
+
+
+def measure_elapsed_time(f):
+    start = timer()
+    f
+    end = timer()
+    print 'start time: ', start, ' end time: ', end, ' duration: ', end - start
+
+
+# X, y = data_utils.load_CIFAR_batch('datasets\cifar-10\data_batch_1')
+# Xte, yte = data_utils.load_CIFAR_batch('datasets\cifar-10\\test_batch')
+X, y, Xte, yte = data_utils.load_CIFAR10('datasets\cifar-10')
 
 X = X.reshape(X.shape[0], 32 * 32 * 3)
 Xte = Xte.reshape(Xte.shape[0], 32 * 32 * 3)
@@ -18,13 +48,8 @@ X = np.insert(X, dim, 1, axis=1)
 Xte = np.insert(Xte, dim, 1, axis=1)
 
 # Nearest Neighbor (NOTE: data pre-processing has no effect on NN)
-# nn = KNearestNeighbor()
-# nn.train(X, y)
-# y_pred = nn.predict(Xte)
-
+measure_elapsed_time(run_knn())
 # LinearSVM
-lc = LinearSVM()
-lc.train(X, y, batch_size=200, num_iters=1)
-y_pred = lc.predict(Xte)
-
-print 'accuracy: %f' % (np.mean(y_pred == yte))
+measure_elapsed_time(run_svm())
+# Softmax
+measure_elapsed_time(run_softmax())
