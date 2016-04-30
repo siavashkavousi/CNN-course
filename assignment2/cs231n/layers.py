f54fbd03ef265ff727351ea13301955d02277246
siavash <siavash.kavousi.sk@gmail.com>
2016-04-30 18:43:39 +0430

    assignment2: Implements backward dropout

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -283,13 +283,7 @@ def dropout_backward(dout, cache):
 
     dx = None
     if mode == 'train':
-        ###########################################################################
-        # TODO: Implement the training phase backward pass for inverted dropout.  #
-        ###########################################################################
-        pass
-        ###########################################################################
-        #                            END OF YOUR CODE                             #
-        ###########################################################################
+        dx = dout * mask
     elif mode == 'test':
         dx = dout
     return dx
